7ad7df17382f9949697e83cd4d665d5e305e5324
MohanManirul <fiforeg@gmail.com>
2023-07-21 11:29:00 +0600

    first commit

---
+++ src/components/Content.jsx
@@ -0,0 +1,19 @@
+
+
+const Content = () => {
+    return (
+        <div className="container">
+
+            <div className="row">
+                <div className="col-md-6 todo-left">
+                col-6
+                </div>
+                <div className="col-md-6 todo-form">
+                col-6
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Content;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div className="headerDiv">
+            <h2 className="header">to do list</h2>
+        </div>
+    );
+};
+
+export default Header;
